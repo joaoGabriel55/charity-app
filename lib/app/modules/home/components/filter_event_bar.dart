2d1b310478ec63b04f7409c7ca20e2701922028b
joaoGabriel55 <j.quaresmasantos_98@hotmail.com>
2020-06-26 22:11:51 -0300

    Added icons and simple label

--- lib/app/modules/home/components/filter_event_bar.dart
+++ lib/app/modules/home/components/filter_event_bar.dart
@@ -13,43 +13,68 @@ class _FilterEventBarState extends State<FilterEventBar> {
       bottom: 0.0,
       left: 104.0,
       right: 0.0,
-      height: 100,
+      height: 108,
       child: Card(
-          elevation: 8.0,
-          margin: EdgeInsets.all(0),
-          shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(28),
-                  topRight: Radius.circular(0),
-                  bottomLeft: Radius.circular(0),
-                  bottomRight: Radius.circular(0))),
-          child: Container(
-              child: ListView(
-            scrollDirection: Axis.horizontal,
-            children: <Widget>[
-              Container(width: 18),
-              FilterEventButton(
-                onPressed: () {},
-                color: Colors.yellow[700],
+        elevation: 8.0,
+        margin: EdgeInsets.all(0),
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(28),
+                topRight: Radius.circular(0),
+                bottomLeft: Radius.circular(0),
+                bottomRight: Radius.circular(0))),
+        child: Container(
+          margin: EdgeInsets.only(top: 8),
+          child: Stack(
+            children: [
+              ListView(
+                scrollDirection: Axis.horizontal,
+                children: <Widget>[
+                  Container(width: 18),
+                  FilterEventButton(
+                    icon: "food",
+                    tooltip: "Distribuição de alimentos",
+                    onPressed: () {},
+                    color: Colors.yellow[700],
+                  ),
+                  Container(width: 18),
+                  FilterEventButton(
+                    icon: "shirt",
+                    tooltip: "Doações de vestimentas",
+                    onPressed: () {},
+                    color: Colors.green[700],
+                  ),
+                  Container(width: 18),
+                  FilterEventButton(
+                    icon: "culture",
+                    tooltip: "Eventos culturais",
+                    onPressed: () {},
+                    color: Colors.purpleAccent[700],
+                  ),
+                  Container(width: 18),
+                  FilterEventButton(
+                    icon: "sports",
+                    tooltip: "Eventos esportivos",
+                    onPressed: () {},
+                    color: Colors.blue[700],
+                  ),
+                  Container(width: 8),
+                ],
               ),
-              Container(width: 18),
-              FilterEventButton(
-                onPressed: () {},
-                color: Colors.green[700],
+              Container(
+                margin: EdgeInsets.only(left: 20),
+                child: Text(
+                  "Selecionar tipo de Ação",
+                  style: TextStyle(
+                      fontSize: 12.0,
+                      color: Theme.of(context).primaryColor,
+                      fontWeight: FontWeight.bold),
+                ),
               ),
-              Container(width: 18),
-              FilterEventButton(
-                onPressed: () {},
-                color: Colors.purpleAccent[700],
-              ),
-              Container(width: 18),
-              FilterEventButton(
-                onPressed: () {},
-                color: Colors.blue[700],
-              ),
-              Container(width: 8),
             ],
-          ))),
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/app/modules/home/components/filter_event_button.dart
+++ lib/app/modules/home/components/filter_event_button.dart
@@ -3,9 +3,15 @@ import 'package:flutter/material.dart';
 class FilterEventButton extends StatefulWidget {
   final Color color;
   final Function onPressed;
+  final String icon;
+  final String tooltip;
 
   const FilterEventButton(
-      {Key key, @required this.color, @required this.onPressed})
+      {Key key,
+      @required this.color,
+      @required this.onPressed,
+      this.icon,
+      this.tooltip})
       : super(key: key);
 
   @override
@@ -16,10 +22,16 @@ class _FilterEventButtonState extends State<FilterEventButton> {
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
+      tooltip: this.widget.tooltip,
       onPressed: this.widget.onPressed,
       foregroundColor: Colors.white,
       backgroundColor: this.widget.color,
-      child: Icon(Icons.search),
+      child: new Image.asset(
+        'assets/images/${this.widget.icon}.png',
+        height: 38,
+        width: 38,
+        filterQuality: FilterQuality.high,
+      ),
       elevation: 0,
     );
   }
